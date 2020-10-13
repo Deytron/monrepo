b32574d950b0514b213f3ee8a0b9ab995c0f3939
Romain Ranaivoson <romain.ranaivoson@ynov.com>
2020-10-13 11:09:06 +0200

    added gitea systemd

--- tp4/scripts/node1.gitea.sh
+++ tp4/scripts/node1.gitea.sh
@@ -1,3 +1,13 @@
 sudo yum update -y
-sudo yum install wget git nano epel-release -y
+sudo yum install wget git nano epel-release mariadb-server -y
+sudo useradd git
+sudo mkdir -p /etc/gitea /var/lib/gitea/{custom,data,indexers,public,log}
+sudo chown git:git /var/lib/gitea/{data,indexers,log}
+sudo chmod 750 /var/lib/gitea/{data,indexers,log}
+sudo chown root:git /etc/gitea
+sudo chmod 770 /etc/gitea
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
+sudo wget https://github.com/go-gitea/gitea/releases/download/v1.12.5/gitea-1.12.5-linux-amd64 -O /usr/local/bin/gitea
+sudo chmod +x /usr/local/bin/gitea
 
